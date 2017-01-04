b21e25f78762027bed93c8a6bf4880ab1a725612
Renat Idrisov <ridrisov@mz.com>
2017-01-04 16:25:57 +0100

    Assert logic

--- common_apps/mzbench_language/src/mzbl_asserts.erl
+++ common_apps/mzbench_language/src/mzbl_asserts.erl
@@ -1,4 +1,4 @@
--module(mzb_asserts).
+-module(mzbl_asserts).
 
 -export([
     validate/1,
@@ -6,11 +6,13 @@
     update_state/2,
     get_failed/3,
     check_loop_expr/1,
+    check_expr/1,
     format_state/1
 ]).
 
--include_lib("mzbench_language/include/mzbl_types.hrl").
+-include_lib("mzbl_types.hrl").
 
+-spec validate(script_expr()) -> list(string()).
 validate(#operation{name = assert, args = [always, Expression], meta = M}) ->
     validate_assert_expr(Expression, M);
 validate(#operation{name = assert, args = [Time, Expression], meta = M}) ->
@@ -21,9 +23,9 @@ validate(#operation{name = assert, args = [Time, Expression], meta = M}) ->
     ++
     validate_assert_expr(Expression, M).
 
-validate_assert_expr(Op) when is_number(Op) or is_list(Op) -> [];
+validate_assert_expr(Op, _M) when is_number(Op) or is_list(Op) -> [];
 validate_assert_expr(#operation{name = Name, args = [Op1, Op2]}, M) ->
-    validate_assert_op(Name, M) ++ validate_assert_expr(Op1) ++ validate_assert_expr(Op2);
+    validate_assert_op(Name, M) ++ validate_assert_expr(Op1, M) ++ validate_assert_expr(Op2, M);
 validate_assert_expr(_Invalid, M) ->
     [mzb_string:format("~sInvalid assert expression", [mzbl_script:meta_to_location_string(M)])].
 
@@ -38,6 +40,7 @@ validate_assert_op(Name, M) ->
     [mzb_string:format("~sInvalid assert operation: ~p",
         [mzbl_script:meta_to_location_string(M), Name])].
 
+-spec get_failed(boolean(), integer(), [map()]) -> [tuple()].
 get_failed(IsFinished, Accuracy, State) ->
     Failed = lists:filter(fun (A) -> not check_assert(IsFinished, Accuracy * 1000, A) end, State),
     [{Expr, format_error(A)} || #{assert_expr:= Expr} = A <- Failed].
@@ -49,6 +52,7 @@ check_assert(_, _Accuracy, #{assert_time:= always}) -> true;
 check_assert(true, _, #{assert_time:= ExpectedTime, satisfy_time:= STime}) when ExpectedTime > STime -> false;
 check_assert(_, _, #{assert_time:= _, satisfy_time:= _}) -> true.
 
+-spec init(list(tuple())) -> [map()].
 init(Asserts) ->
     [#{assert_time => always,
        assert_expr => Expr,
@@ -59,6 +63,7 @@ init(Asserts) ->
        satisfy_time => 0,
        unsatisfy_time => 0} || {#constant{value = Time, units = ms}, Expr} <- Asserts].
 
+-spec update_state(integer(), [map()]) -> [map()].
 update_state(TimeSinceCheck, State) ->
     lists:map(
         fun (#{assert_expr:= Expr, satisfy_time:= STime, unsatisfy_time:= UTime} = A) ->
@@ -68,12 +73,14 @@ update_state(TimeSinceCheck, State) ->
             end
         end, State).
 
+-spec check_loop_expr(list()) -> boolean().
 check_loop_expr(List) -> lists:all(fun check_expr/1, List).
 
 get_value(V) when is_number(V) -> V;
 get_value(Metric) when is_list(Metric) -> mzb_metrics:get_value(Metric).
 
-check_expr(#operation{name = 'not', args = [Exp1]}) -> !check_expr(Exp1);
+-spec check_expr(any()) -> boolean().
+check_expr(#operation{name = 'not', args = [Exp1]}) -> not check_expr(Exp1);
 check_expr(#operation{name = 'and', args = [Exp1, Exp2]}) ->
     case check_expr(Exp1) of
         false -> false;
@@ -88,7 +95,7 @@ check_expr(#operation{name = Op, args = [Value1, Value2]}) ->
     try {get_value(Value1), get_value(Value2)} of
         {V1, V2} -> check_value(Op, V1, V2)
     catch
-        error:{badarg, Metric, _} -> false
+        error:{badarg, _Metric, _} -> false
     end.
 
 check_value(gt, V1, V2) -> V1 > V2;
@@ -132,6 +139,7 @@ format_time(Time) ->
         {{H, M, S}, _} -> io_lib:format("~bh ~bm ~bs", [H, M, S])
     end.
 
+-spec format_state([map()]) -> string().
 format_state([]) -> "(empty)";
 format_state(State) ->
     Lines = lists:map(
